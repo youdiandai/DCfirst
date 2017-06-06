e51081dd68d84888250b2df9823d7ddcbdf97a8a
ChangXin <1561753947@qq.com>
2017-06-06 22:15:55 +0800

    database

--- hello.py
+++ hello.py
@@ -8,26 +8,28 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 manager = Manager(app)
 app.config['SECRET_KEY'] ='synudc'
-dbaddress = 'mysql://root:aizai2017@127.0.0.1/dc'
-app.config['SQLALCHEMY_DATABASE_URL']=dbaddress
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:aizai2017@localhost:3306/dc?charset=utf8mb4'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-db = SQLAlchemy(app)
+db = SQLAlchemy(app,use_native_unicode="utf8")
 
 class User(db.Model):
     __tablename__ = 'user'
     userid = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64),unique=True,nullable=False)
     name = db.Column(db.String(64),nullable=False)
-    password = db.Column(db.Integer,nullable=False)
+    password = db.Column(db.String(64),nullable=False)
     collage = db.Column(db.String(64), nullable=False)
     tel = db.Column(db.Integer,nullable=False)
-    usermode = db.Column(db.String(64),nullable=False)
+    usermode = db.Column(db.Integer,db.ForeignKey('user_mode.mid'))
+    linkp = db.relationship('User_Project',backref='user')
     def __repr__(self):
         return '<User %r>' % self.username
 
 class User_mode(db.Model):
-    __tablename__ = 'user_name'
+    __tablename__ = 'user_mode'
     mid = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64),unique=True)
+    users = db.relationship('User',backref='mode')
     def __repr__(self):
         return '<User_name %r>' % self.name
 
@@ -37,29 +39,31 @@ class Project(db.Model):
     pname = db.Column(db.Text,nullable=False)
     Person_in_charge = db.Column(db.Integer,nullable=False)
     docurl = db.Column(db.Text)
+    linku = db.relationship('User_Project',backref='project')
     def __repr__(self):
         return  '<Project %r>'%self.pid
 
 class User_Project(db.Model):
     __tablename__ = 'User_Project'
-    userid = db.Column(db.Integer,primary_key=True)
-    pid = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
+    userid = db.Column(db.Integer,db.ForeignKey('user.userid'))
+    pid = db.Column(db.Integer,db.ForeignKey('project.pid'))
     def __repr__(self):
         return '<User_Project %r %r>'%self.userid,self.pid
 
 
 class Login(Form):
-    username = StringField("用户名",validators=[Required()])
-    password = PasswordField("密码",validators=[Required()])
+    username = StringField("用户名" ,validators=[Required()])
+    password = PasswordField("密码" ,validators=[Required()])
     mode = RadioField('用户类型', choices=[('admin', '管理员'), ('user', '用户')])
     submit = SubmitField('登录')
 
 class Register(Form):
-    username = StringField("学号",validators=[Required()])
-    name = StringField("姓名",validators=[Required()])
-    password = PasswordField("密码",validators=[Required()])
+    username = StringField("学号" ,validators=[Required()])
+    name = StringField("姓名" ,validators=[Required()])
+    password = PasswordField("密码" ,validators=[Required()])
     repassword = PasswordField("再次输入密码",validators=[Required()])
-    collage = StringField("学院",validators=[Required()])
+    collage = StringField("学院" ,validators=[Required()])
     tel = StringField("电话号码")
     submit = SubmitField('注册')
 
@@ -81,13 +85,13 @@ def login():
 def register():
     register = Register()
     if register.validate_on_submit():
-        session['username']=register.username.data
-        session['name']=register.name.data
-        session['password']=register.password.data
-        session['repassword']=register.repassword.data
-        session['collage']=register.collage.data
-        session['tel']=register.tel.data
-        return  redirect(url_for('register'))
+        username =User.query.filter_by(username=register.username.data).first()
+        if username is None:
+            session['password']=register.password.data
+            session['repassword']=register.repassword.data
+            session['collage']=register.collage.data
+            session['tel']=register.tel.data
+            return  redirect(url_for('register'))
     return render_template('register.html',form=register)
 
 if __name__ == '__main__':
