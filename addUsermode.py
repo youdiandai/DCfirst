89e6c87fc48359ab27ac2a635a5a18efb72be3a7
ChangXin <1561753947@qq.com>
2017-08-15 03:36:24 +0800

    完成了项目立项，项目中期报告，项目结题报告的提交

--- addUsermode.py
+++ addUsermode.py
@@ -1,9 +1,9 @@
 #-*- encoding:utf-8 -*-
 from hello import User_mode,db
-judge = User_mode(name='评委老师')
+judge = User_mode(name='学生')
 manager = User_mode(name='管理员')
-student = User_mode(name='学生')
-proleader = User_mode(name='项目负责人')
+student = User_mode(name='教师用户')
+proleader = User_mode(name='学院管理员')
 db.session.add(judge)
 db.session.add(manager)
 db.session.add(student)
--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,session,make_response,send_file,request
 from flask.ext.script import Manager
 from flask.ext.wtf import Form
 from wtforms import StringField,SubmitField,PasswordField,RadioField,TextAreaField,SelectField,FileField
-from wtforms.validators import DataRequired,equal_to
+from wtforms.validators import DataRequired,equal_to,Email
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import time
@@ -30,11 +30,12 @@ class User(db.Model):
     __tablename__ = 'user'
     userid = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(64),unique=True,nullable=False)
-    name = db.Column(db.String(64),nullable=True)
+    name = db.Column(db.String(64),nullable=False)
     password = db.Column(db.String(64),nullable=False)
     collage = db.Column(db.String(64), nullable=True)
     major = db.Column(db.String(64), nullable=True)
     tel = db.Column(db.String(64),nullable=True)
+    email = db.Column(db.String(64),nullable=True)
     usermode = db.Column(db.Integer,db.ForeignKey('user_mode.mid'))
     linkp = db.relationship('User_Project',backref='user')
     def __repr__(self):
@@ -64,18 +65,105 @@ class User_mode(db.Model):
 #项目
 class Project(db.Model):
     __tablename__ = 'project'
-    pid = db.Column(db.Integer,primary_key=True)
-    pname = db.Column(db.String(64),nullable=False)
-    plevel = db.Column(db.String(64))
-    collage = db.Column(db.String(64))
-    Person_in_charge = db.Column(db.Integer)
-    teacher = db.Column(db.String(64))
-    describe = db.Column(db.Text)
-    status = db.Column(db.Integer)
-    doc = db.Column(db.String(64))
+    pid = db.Column(db.Integer,primary_key=True)#项目编号
+    Pname = db.Column(db.String(64),nullable=False)#项目名称
+    Plevel = db.Column(db.String(64),nullable=True)#项目级别
+    Pclass = db.Column(db.String(64),nullable=True)#项目类别
+    Collage = db.Column(db.String(64),nullable=True)#学院
+    Person_in_charge = db.Column(db.Integer,nullable=True)#负责人
+    Teacher = db.Column(db.String(64),nullable=True)#指导教师
+    Forteacher = db.Column(db.String(64),nullable=True)#校外导师
+    Describe = db.Column(db.Text,nullable=True)#项目简介
+    Status = db.Column(db.Integer,nullable=True)#项目状态
+    StartDate = db.Column(db.String(64),nullable=True)#立项日期
+    PlanDate = db.Column(db.String(64),nullable=True)#预计结束日期
+    MidSubdate = db.Column(db.String(64),nullable=True)#中期报告提交日期
+    EndSubdate = db.Column(db.String(64),nullable=True)#结题提交日期
+    EndDate = db.Column(db.String(64),nullable=True)#项目结束日期
+    ReassonsForApplication = db.Column(db.String(64),nullable=True)#申请理由
+    ProjectPlan = db.Column(db.String(64),nullable=True)#项目方案
+    Innovate = db.Column(db.Text,nullable=True)#特色和创新点
+    Schedule = db.Column(db.Text,nullable=True)#项目进度安排
+    Budget = db.Column(db.Integer,nullable=True)#项目经费预算
+    BudgetPlan = db.Column(db.Text,nullable=True)#经费使用计划
+    ExpectedResults = db.Column(db.Text,nullable=True)#项目预期成果
+    TeaStarOpinion = db.Column(db.Text,nullable=True)#指导教师意见（立项）
+    CollStarOpinion = db.Column(db.Text,nullable=True)#学院意见
+    SchStarOpinion = db.Column(db.Text,nullable=True)#学校意见（立项）
+    MidProgress= db.Column(db.Text,nullable=True)#工作进展
+    MidResults = db.Column(db.Text,nullable=True)#中期成果
+    TeaMidOpinion = db.Column(db.Text,nullable=True)#指导教师意见（中期）
+    DCCenterMidOpinion = db.Column(db.Text,nullable=True)#大创中心意见（中期）
+    SchMidOpinion = db.Column(db.Text,nullable=True)#学校意见（中期）
+    ResultsDescribe = db.Column(db.Text,nullable=True)#项目成果简介
+    ResultsType = db.Column(db.Integer,nullable=True)#项目成果形式
+    ResultsSummary = db.Column(db.Text,nullable=True)#项目总结报告
+    ResultsNum = db.Column(db.Integer,nullable=True)#成果编号
+    Problems = db.Column(db.Text,nullable=True)#存在的问题和建议
+    TeaEndOpinion = db.Column(db.Text,nullable=True)#项目指导教师意见（结题）
+    DCCenterEndOpinion = db.Column(db.Text,nullable=True)#大创中心意见（结题）
+    Achievement = db.Column(db.String(64),nullable=True)#成绩
+    doc = db.Column(db.String(64),nullable=True)#结题成果
     linku = db.relationship('User_Project',backref='project')
     def __repr__(self):
         return  '<Project %r>'%self.pid
+
+#资金
+class Funds(db.Model):
+    __tablename__ = "funds"
+    Fid = db.Column(db.Integer,primary_key=True)
+    FundingUse = db.Column(db.Text)
+    Amount = db.Column(db.Integer)
+    Pid = db.Column(db.Integer)
+
+#成果形式
+class ResultsType(db.Model):
+    __tablename__ = "ResultsType"
+    Rid = db.Column(db.Integer,primary_key=True)
+    Rname = db.Column(db.String(64))
+
+#成果，专利
+class Patent(db.Model):
+    __tablename__ = "patent"
+    patentId = db.Column(db.Integer,primary_key=True)
+    patentName = db.Column(db.String(64))
+    person = db.Column(db.String(64))
+    number = db.Column(db.String(64))
+    remarks = db.Column(db.String(64))
+#成果，论文
+class Paper(db.Model):
+    __tablename__ = "paper"
+    paperId = db.Column(db.Integer,primary_key=True)
+    subject = db.Column(db.String(64))
+    author = db.Column(db.String(64))
+    JournalName = db.Column(db.String(64))
+    remarks = db.Column(db.String(64))
+#成果，报告
+class Report(db.Model):
+    __tablename__ = "report"
+    reportId = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64))
+    author = db.Column(db.String(64))
+    remarks = db.Column(db.String(64))
+#成果，商业计划书
+class BusinessPlan(db.Model):
+    __tablename__ = "BusinessPlan"
+    BusinessPlanId = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64))
+    remarks = db.Column(db.String(64))
+#成果，网站
+class Website(db.Model):
+    __tablename__="Website"
+    WebId = db.Column(db.Integer,primary_key=True)
+    website = db.Column(db.String(64))
+    remarks = db.Column(db.String(64))
+#成果，其他
+class Other(db.Model):
+    __tablename__ = "other"
+    OtherId = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64))
+    remarks = db.Column(db.String(64))
+
 
 #项目状态
 class Project_mode(db.Model):
@@ -108,6 +196,7 @@ class Register(Form):
     collage = SelectField("学院" ,validators=[DataRequired()],choices=[(x.cname,x.cname) for x in Collage.query.all()])
     major = SelectField("专业", validators=[DataRequired()],choices=[(x.mname,x.mname) for x in Major.query.all()])
     tel = StringField("电话号码")
+    email = StringField("E-mail",validators=[Email()])
     submit = SubmitField('注册')
 
 #创建项目页面的表单
@@ -119,6 +208,38 @@ class Create_project(Form):
     describe = TextAreaField("项目简介")
     file = FileField("文档")
     submit = SubmitField('创建')
+#立项申请
+class ProjectApproval(Form):
+    projectname = StringField("项目名")
+    PlanDate = StringField("预计结束日期")
+    member = StringField("项目成员")#中间用逗号分隔
+    Teacher = StringField("指导教师")#指导教师
+    collage = SelectField("项目所属学院" ,validators=[DataRequired()],choices=[(x.cname,x.cname) for x in Collage.query.all()])
+    Describe = TextAreaField("项目简介")
+    projectclass = RadioField('项目分类', choices=[('创新训练项目', '创新训练项目'), ('创业训练项目', '创业训练项目'),('创业实践项目', '创业实践项目')])
+    ReassonsForApplication = TextAreaField("申请理由")
+    ProjectPlan = TextAreaField("项目方案")
+    Innovate = TextAreaField("项目特色和创新点")
+    Schedule = TextAreaField("项目进度安排")
+    Budget = StringField("项目经费")
+    BudgetPlan = TextAreaField("经费使用计划")
+    ExpectedResults = TextAreaField("项目预期成果物")
+    submit = SubmitField('提交')
+
+#中期申报表单
+class ProjectMid(Form):
+    MidProgress = TextAreaField("研究工作进展情况")
+    file = FileField("提交成果物")
+    submit = SubmitField('提交')
+
+#结题报告表单
+class ProjectEnd(Form):
+    ResultsDescribe = TextAreaField("项目成果简介")
+    ResultsSummary = TextAreaField("项目总结报告")
+    Problems = TextAreaField("项目遇到的问题和建议")
+    file = FileField("提交成果物")
+    submit = SubmitField('提交')
+
 
 #加入项目页面的表单
 class Join_project(Form):
@@ -142,6 +263,10 @@ def createStatuslist():
 #根据提供的用户名获取用户现在的权限
 def getUserauth(user):
    return User.query.filter_by(username=user).first().usermode
+
+#根据状态名获取项目状态号
+def getProStatusSid(status):
+    return Project_mode.query.filter_by(status=status).first().sid
 
 #路由
 @app.route('/')
@@ -193,6 +318,7 @@ def register():
             user.collage=register.collage.data
             user.major=register.major.data
             user.tel = register.tel.data
+            user.email = register.email.data
             user.usermode = User_mode.query.filter_by(name='学生').first().mid
 
             db.session.add(user)
@@ -203,26 +329,27 @@ def register():
     return render_template('register.html',form=register)
 
 #完成创建项目功能
+"""
 @app.route('/createproject.html',methods=['GET','POST'])
 def create_project():
     form=Create_project()
     if form.validate_on_submit():
-        pname = Project.query.filter_by(pname=form.projectname.data).first()
+        pname = Project.query.filter_by(Pname=form.projectname.data).first()
         if pname is None:
             #增加各种数据项到数据库
             pro = Project()
             pu=User_Project()
-            pro.pname = form.projectname.data
-            pro.plevel = form.projectlevel.data
-            pro.describe = form.describe.data
+            pro.Pname = form.projectname.data
+            pro.Plevel = form.projectlevel.data
+            pro.Describe = form.describe.data
             pro.Person_in_charge=session["username"]
-            pro.collage = form.collage.data
-            pro.teacher = form.teacher.data
-            pro.status = Project_mode.query.filter_by(status='未审核').first().sid
+            pro.Collage = form.collage.data
+            pro.Teacher = form.teacher.data
+            pro.Status = Project_mode.query.filter_by(status='未审核').first().sid
             db.session.add(pro)
             db.session.commit()
-            pu.pid=Project.query.filter_by(pname=form.projectname.data).first().pid
-            pu.userid=User.query.filter_by(username=Project.query.filter_by(pname=form.projectname.data).first().Person_in_charge).first().userid
+            pu.pid=Project.query.filter_by(Pname=form.projectname.data).first().pid
+            pu.userid=User.query.filter_by(username=Project.query.filter_by(Pname=form.projectname.data).first().Person_in_charge).first().userid
             #完成文件上传功能
             file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])+'/start'
             if not os.path.exists(file_dir):
@@ -241,17 +368,115 @@ def create_project():
         else:
             return '创建失败'
     return render_template('createproject.html', form=form)
+"""
+
+#项目申报
+@app.route('/project_application.html',methods=['GET'])
+def project_application():
+    return render_template('project_application.html')
+
+@app.route('/project_application_content.html',methods=['GET','POST'])
+def project_application_content():
+    form = ProjectApproval()
+    if form.validate_on_submit():
+        pname = Project.query.filter_by(Pname=form.projectname.data).first()
+        if pname is None:
+            pro = Project()
+            pu = User_Project()
+            pro.Pname = form.projectname.data
+            pro.PlanDate = form.PlanDate.data
+            pro.Status = Project_mode.query.filter_by(status='等待指导教师审核').first().sid
+            pro.Collage = form.collage.data
+            pro.Teacher = form.Teacher.data
+            pro.Describe = form.Describe.data
+            pro.Pclass = form.projectclass.data
+            pro.ReassonsForApplication = form.ReassonsForApplication.data
+            pro.ProjectPlan = form.ProjectPlan.data
+            pro.Innovate = form.Innovate.data
+            pro.Schedule = form.Schedule.data
+            pro.Budget = form.Budget.data
+            pro.BudgetPlan = form.BudgetPlan.data
+            pro.ExpectedResults = form.ExpectedResults.data
+            pro.Person_in_charge = session["username"]
+            db.session.add(pro)
+            db.session.commit()
+            pu.pid=Project.query.filter_by(Pname=form.projectname.data).first().pid
+            pu.userid=User.query.filter_by(username=Project.query.filter_by(Pname=form.projectname.data).first().Person_in_charge).first().userid
+            db.session.add(pu)
+            db.session.commit()
+            for x in form.member.data.split(","):
+                db.session.add(User_Project(pid=Project.query.filter_by(Pname=form.projectname.data).first().pid,userid=User.query.filter_by(username=x).first().userid))
+                db.session.commit()
+            return '创建成功'
+        else:
+            return "项目已存在"
+    return render_template('project_application_content.html',form=form)
+#中期申报
+@app.route('/interim_report.html',methods=['GET'])
+def interim_report():
+    return render_template('interim_report.html')
+@app.route('/interim_report_content.html',methods=['GET','POST'])
+def interim_report_content():
+    form = ProjectMid()
+    if form.validate_on_submit():
+        pro = Project.query.filter_by(pid=session['project']).first()
+        pro.MidProgress = form.MidProgress.data
+
+        file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+        # 重命名文件
+        fname = secure_filename(form.file.data.filename).split('.', 1)[-1]
+        unix_time = int(time.time())
+        new_filename = pro.Pname+'Mid'+ '.' + fname
+
+        form.file.data.save('upload/'+ new_filename)
+        pro.MidResults = new_filename
+        pro.Status = Project_mode.query.filter_by(status='等待指导教师中期审核').first().sid
+        db.session.add(pro)
+        db.session.commit()
+        return '提交成功'
+    return render_template('interim_report_content.html',form=form)
+#结题报告
+@app.route('/concluding_report.html',methods=['GET'])
+def concluding_report():
+    return render_template('concluding_report.html')
+@app.route('/concluding_report_content.html',methods=['GET','POST'])
+def concluding_report_content():
+    form = ProjectEnd()
+    if form.validate_on_submit():
+        pro = Project.query.filter_by(pid=session['project']).first()
+        pro.ResultsDescribe = form.ResultsDescribe.data
+        pro.ResultsSummary = form.ResultsSummary.data
+        pro.Problems = form.Problems.data
+        file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+        # 重命名文件
+        fname = secure_filename(form.file.data.filename).split('.', 1)[-1]
+        unix_time = int(time.time())
+        new_filename = pro.Pname+'End'+ '.' + fname
+
+        form.file.data.save('upload/'+ new_filename)
+        pro.doc = new_filename
+        pro.Status = Project_mode.query.filter_by(status='等待指导教师结题审核').first().sid
+        db.session.add(pro)
+        db.session.commit()
+        return '提交成功'
+    return render_template('concluding_report_content.html',form=form)
+
+
 
 #完成用户加入项目功能
 @app.route('/join_project',methods=['GET','POST'])
 def join_project():
     form = Join_project()
     if form.validate_on_submit():
-        pud = User_Project.query.filter_by(pid=Project.query.filter_by(pname=form.projectname.data).first().pid,userid=User.query.filter_by(username=session["username"]).first().userid).first()
+        pud = User_Project.query.filter_by(pid=Project.query.filter_by(Pname=form.projectname.data).first().pid,userid=User.query.filter_by(username=session["username"]).first().userid).first()
         if pud is None:
             pu=User_Project()
             pu.userid=User.query.filter_by(username=session["username"]).first().userid
-            pu.pid=Project.query.filter_by(pname=form.projectname.data).first().pid
+            pu.pid=Project.query.filter_by(Pname=form.projectname.data).first().pid
             db.session.add(pu)
             db.session.commit()
             return '加入成功'
@@ -272,7 +497,7 @@ def project(pid):
         auth=False
     psta = None
     for x in createStatuslist():#生成用来判断显示什么按钮的变量
-        if pro.status == x[0]:
+        if pro.Status == x[0]:
             psta = (x[0],x[1]) #x[0]为状态号，x[1]为状态名
     return render_template('project.html',pro =pro ,pchar=pchar,pmembers=pmembers,auth=auth,pstatus=psta)
 
@@ -282,19 +507,19 @@ def auth(num):
     info=None
     pro=Project.query.filter_by(pid=session['project']).first()
     if Project_mode.query.filter_by(sid=num).first().status == '未审核':
-        pro.status=Project_mode.query.filter_by(status='未完成').first().sid
+        pro.Status=Project_mode.query.filter_by(status='未完成').first().sid
         info='审核成功'
     elif Project_mode.query.filter_by(sid=num).first().status == '未完成':
-        pro.status=Project_mode.query.filter_by(status='完成').first().sid
+        pro.Status=Project_mode.query.filter_by(status='完成').first().sid
         info='项目完成'
     db.session.add(pro)
     db.session.commit()
     return render_template('authsucc.html',info=info)
 
-#下载初始文件路由
-@app.route('/start/<filename>', methods=['GET'])
+#下载文件路由
+@app.route('/download/<filename>', methods=['GET'])
 def startdownload(filename):
-    response = make_response(send_file(basedir+"/upload/"+'start'+'/'+filename))
+    response = make_response(send_file(basedir+"/upload/"+filename))
     response.headers["Content-Disposition"] = "attachment; filename="+filename+";"
     return response
 
