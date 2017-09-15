5d088637c75e7c4341d2c1b701a9093f02bf9d6a
ChangXin <1561753947@qq.com>
2017-09-15 22:42:51 +0800

    修改了结题报告上传文件设置的文件名

--- hello.py
+++ hello.py
@@ -536,7 +536,7 @@ def concluding_report_content():
         # 重命名文件
         fname = secure_filename(form.file.data.filename).split('.', 1)[-1]
         unix_time = int(time.time())
-        new_filename = pro.Pname+'End'+ '.' + fname
+        new_filename = pro.Pid+'End'+ '.' + fname
 
         form.file.data.save('upload/'+ new_filename)
         pro.doc = new_filename
